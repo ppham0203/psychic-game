039d7f5dfbcd920889b6a5b1bb973764aba0b9cf
Peter Pham <ppham0203@Peters-MacBook-Pro.local>
2017-07-27 23:56:43 -0700

    updated

--- psychic.js
+++ psychic.js
@@ -1,3 +1,4 @@
+
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
@@ -7,17 +8,18 @@ var guesses = 10;
 var guessedLetters = [];
 
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    console.log(computerChoice);
 
+var computerChoice = alphabet[Math.floor(Math.random() * 26 ) +1];
+
+console.log(computerChoice);
 
 
    // When the user presses a key, it will run the following function...
 document.onkeypress = function(event) {
     var userGuess = event.key;
 guessedLetters.push(userGuess);
- 
+
  
 
     if(userGuess === computerChoice){
@@ -25,10 +27,11 @@ guessedLetters.push(userGuess);
         guesses = 10;
         guessedLetters = [];
         alert("Correct, you win!");
-        v
+       
 
     }else{
         guesses--;
+        
     }
 
     if(guesses === 0){
@@ -36,15 +39,15 @@ guessedLetters.push(userGuess);
         alert("Sorry, you lose!");
         guesses = 10;
         guessedLetters = []
- 
-
+        
 
     }
 
     
     document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "<h2>losses:</h2> " + losses;
+    document.getElementById('losses').innerHTML = "losses: " + losses;
     document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
     document.querySelector('#wrong').innerHTML = "Letters you've guessed: " + guessedLetters.join(' ');
 }
+
 
